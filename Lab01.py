a97bb84218aadbdfe05928654fe6b9feb4a7ffd7
aqib-mehmood <aqimehmoodb@gmail.com>
2020-10-25 23:09:06 +0500

    Lab 01 python - Subject AI

---
+++ Lab01.py
@@ -0,0 +1,18 @@
+# Task 01
+# Take user information as an input from user and print his bio using python.
+
+
+print("Welcome to python programming")
+print("Please Enter your Information below:")
+fname = input("Your First Name : ")
+lname = input("Your Last Name : ")
+gender = input("Enter Gender info : ")
+dob = input("Your DOB : ")
+email = input("Your Email Addres : ")
+
+print("--------Your Bio Data--------")
+print(f"First Name: {fname}")
+print(f"Last Name: {lname}")
+print(f"Gender: {gender}")
+print(f"DOB: {dob}")
+print(f"Email: {email}")
